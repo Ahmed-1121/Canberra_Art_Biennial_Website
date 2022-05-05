0fb222ab4c208f7a787ed72e4104979cc7b0336b
Ahmed-1121 <100173139+Ahmed-1121@users.noreply.github.com>
2022-05-06 02:16:23 +1000

    fix js

--- JavaScript.js
+++ JavaScript.js
@@ -1,15 +1,28 @@
 //hamburger menu: this code allow to show and hide menu in mobile & tablet viws.
 //In mobile and tablet viws you will see a hamburger icon appear instead of regular menu.
 //when you press the hamburger icon a menu will show, and iside it you will find a (x) icon, which can be used to close the menu
-function show() {
-  var x =  document.getElementById("show_menu2");
+//function show() {
+  //var x =  document.getElementById("show_menu2");
    
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  //if (x.style.display === "block") {
+    //x.style.display = "none";
+  //} else {
+    //x.style.display = "block";
+//  }
+//}
+
+
+
+function show2() {
+    document.getElementById("show_menu2").classList.remove("show_menu");
+    document.getElementById("show_menu2").classList.add("show_menu2");
+    
 }
 
 
+function show() {
+    document.getElementById("show_menu2").classList.remove("show_menu2");
+    document.getElementById("show_menu2").classList.add("show_menu");
+    
+}
 
