d8a6d528cc0c877e0c040fa152c717ba76208d72
Ahmed-1121 <100173139+Ahmed-1121@users.noreply.github.com>
2022-05-05 19:37:31 +1000

    card2

--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,6 @@
-//hamburger menu
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
+//hamburger menu: this code allow to show and hide menu in mobile & tablet viws.
+//In mobile and tablet viws you will see a hamburger icon appear instead of regular menu.
+//when you press the hamburger icon a menu will show, and iside it you will find a (x) icon, which can be used to close the menu
 function show() {
   var x =  document.getElementById("show_menu2");
    
@@ -20,35 +13,3 @@ function show() {
 
 
 
-function bold() {
-    var x = document.getElementById("boldthis");
-    x.style.fontWeight = "bold";
-}
-
-function unbold() {
-    var x = document.getElementById("boldthis");
-    x.style.fontWeight = "400";
-}
-
-
-function bold2() {
-    var x = document.getElementById("boldthis2");
-    x.style.fontWeight = "bold";
-}
-
-function unbold2() {
-    var x = document.getElementById("boldthis2");
-    x.style.fontWeight = "400";
-}
-
-
-
-function bold3() {
-    var x = document.getElementById("boldthis3");
-    x.style.fontWeight = "bold";
-}
-
-function unbold3() {
-    var x = document.getElementById("boldthis3");
-    x.style.fontWeight = "400";
-}
